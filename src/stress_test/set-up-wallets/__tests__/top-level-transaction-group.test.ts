45974901782f181a7ab7ce5345b5ad12e0c39cac
German Ivanov <germivanov@gmail.com>
2019-10-28 00:08:31 +0200

    test: adapt test cases to new logic

--- src/stress_test/set-up-wallets/__tests__/top-level-transaction-group.test.ts
+++ src/stress_test/set-up-wallets/__tests__/top-level-transaction-group.test.ts
@@ -2,14 +2,14 @@ import {getTotalMoneyNeeded} from '../top-level-transaction-group'
 
 describe('getTotalMoneyNeeded', () => {
   it('should return valid resut for depth 1', () => {
-    expect(getTotalMoneyNeeded(50, 1, 0.01, 1)).toBe(7626)
+    expect(getTotalMoneyNeeded(50, 1, 0.01, 1)).toBe(7652)
   })
 
   it('should return valid resut for depth 2', () => {
-    expect(getTotalMoneyNeeded(80, 2, 0.01, 1)).toBe(1220081)
+    expect(getTotalMoneyNeeded(80, 2, 0.01, 1)).toBe(1224162)
   })
 
   it('should return valid resut for depth 3', () => {
-    expect(getTotalMoneyNeeded(15, 3, 0.01, 1)).toBe(22876516)
+    expect(getTotalMoneyNeeded(15, 3, 0.01, 1)).toBe(22953031)
   })
 })
